d41151dc43f424f200b59f354a5b7bef19283783
pro100mq <144606932+pro100mq@users.noreply.github.com>
2025-03-23 15:20:39 +0200

    homework

---
+++ index.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('.slider__input');
+const image = document.querySelector('.slider__image');
+
+input.addEventListener("input", _.debounce((e) => {
+    const scale = e.target.value / 100;
+    image.style.transform = `scale(${scale})`;
+}, 200));
+
+
+const box = document.querySelector("#box")
+box.addEventListener("mousemove", _.debounce((e) => {
+    box.style.left = `${e.pageX - 25}px`;
+    box.style.top = `${e.pageY +- 25}px`;
+}))
